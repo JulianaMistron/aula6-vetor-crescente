4f79ccb9e43237ef2ce802ca06df372b0f650c50
JulianaMistron <jumistron@yahoo.com.br>
2024-04-23 15:35:34 -0300

    cria vetor crescente

--- aula6-vetor-crescente/Program.cs
+++ aula6-vetor-crescente/Program.cs
@@ -1,21 +1,35 @@
 ﻿//faça um programa que leia 5 numeros, quarde os num vetor e ordene os de forma crescente em um novo vetor
+int size = 50, aux = 0;
+int[] vetorOriginal = new int[size];
+int[] vetorOrdenado = new int[size];
 
-int[] numeros = new int[5];
-
-for (int i = 0; i < 5; i++)
+for (int i = 0; i < size; i++)
 {
-    Console.WriteLine("Digite o {0}° número: ", i + 1);
-    numeros[i] = int.Parse(Console.ReadLine());
+    vetorOriginal[i] = new Random().Next(0, 50);
+    vetorOrdenado[i] = vetorOriginal[i];
+}
+Console.WriteLine("Vetor original");
+for (int i = 0; i < size; i++)
+    Console.Write(vetorOriginal[i] + " ");
+
+Console.ReadLine();
+
+for (int referencia = 0; referencia < size; referencia++)//laço de referencia = 1
+{
+    for (int comparacao = referencia + 1; comparacao < size; comparacao++)//laço de comparação = j
+    {
+        if (vetorOrdenado[referencia] > vetorOrdenado[comparacao])
+        {
+            aux = vetorOrdenado[referencia];
+            vetorOrdenado[referencia] = vetorOrdenado[comparacao];
+            vetorOrdenado[comparacao] = aux;
+        }
+    }
 }
 
-int n1 = 0, n2 = 0, n3 = 0, n4 = 0, n5 = 0;
 
-for (int i = 0; i < 5; i++)
-{
-    Console.Write(numeros[i] + " ");
-}
-Array.Sort(numeros);
-foreach (int i in numeros)
-{
-    Console.Write(" " + i);
-}
+Console.WriteLine("Ordenado");
+for (int i = 0; i < size; i++)
+    Console.Write(vetorOrdenado[i] + " ");
+
+Console.ReadLine();
