14e7441eb62a7771f7221fdff8b45775959f51ac
JulianaMistron <jumistron@yahoo.com.br>
2024-04-22 23:55:26 -0300

    Add project files.

---
+++ aula6-vetor-crescente/Program.cs
@@ -0,0 +1,21 @@
+﻿//faça um programa que leia 5 numeros, quarde os num vetor e ordene os de forma crescente em um novo vetor
+
+int[] numeros = new int[5];
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine("Digite o {0}° número: ", i + 1);
+    numeros[i] = int.Parse(Console.ReadLine());
+}
+
+int n1 = 0, n2 = 0, n3 = 0, n4 = 0, n5 = 0;
+
+for (int i = 0; i < 5; i++)
+{
+    Console.Write(numeros[i] + " ");
+}
+Array.Sort(numeros);
+foreach (int i in numeros)
+{
+    Console.Write(" " + i);
+}
